451fc881a7c150796d5a7b6a8294622066868705
SaebaRyoXYZ <90305399+SaebaRyoXYZ@users.noreply.github.com>
2022-11-21 00:32:31 +0530

    ADDED

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function trial(){
+  window.location="https://saebaryoxyz.github.io/BW-TRIAL/";
+}
+
+function download(){
+  window.location="https://drive.google.com/file/d/1Tc6lMwd0hAZGRmQrIyk3VUTCRSsJ8Vw8/view?usp=sharing";
+}
